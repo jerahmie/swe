aa6c9bf4ed9b338c311a74e777daa7f17eb5d774
Jerahmie Radder <radd0012@umn.edu>
2023-03-11 00:17:31 -0600

    added Rust routine to create netcdf source 2D Gaussian

---
+++ util/swesource/src/main.rs
@@ -0,0 +1,20 @@
+use swesource::gaussian2d;
+use netcdf;
+
+
+fn main(){
+    let nx = 10;
+    let ny = 10;
+    let g2 = gaussian2d(0.2, 0.5, 0.5, 0.1, 0.1, nx, ny);
+
+    // create netcdf file
+    let mut file = netcdf::create("gaussian2d.nc").expect("Could not create NetCDF file.");
+
+    // declare dimensions
+    file.add_dimension("nx", nx).expect("Could not add dimension 'nx'");
+    file.add_dimension("ny", ny).expect("Could not add dimension 'ny'");
+
+    // declare variables
+    let mut var = file.add_variable::<f64>("h", &["nx", "ny"]).unwrap();
+    var.put_values(&g2.into_raw_vec().as_slice(), ..).unwrap();
+}
