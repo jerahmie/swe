6cbb7adc8307565fb5d3135538f995c9bf2498d9
Jerahmie Radder <radd0012@umn.edu>
2023-03-16 22:57:49 -0500

    added read subset of PnetCDF dataset into a local h

--- src/halos.f90
+++ src/halos.f90
@@ -10,12 +10,18 @@ program swe_mpi
 
     type(Neighbors) :: cell_neighbors
 
-    integer :: rank, size, ierror, tag, status(MPI_STATUS_SIZE)
-    integer :: ncid
+    integer :: rank, comm_size, tag, status(MPI_STATUS_SIZE)
+    integer :: ncid, ncstatus
+    integer :: i, j, ndims
+    integer :: nx, ny, local_nx, local_ny, nsubgrid
+    integer, dimension(1024) :: vardimids
     character(255) :: cwd, ncfilename
-    character(*), parameter :: ncfile_rel = "/../util/swesource_py/testpy.nc"
+    character(*), parameter :: ncfile_rel = "/../util/swesource_netcdf3/gaussian2d.nc"
+    character(len=32), dimension(:), allocatable :: varname, dimnamei
+    integer(kind=MPI_OFFSET_KIND), dimension(:), allocatable :: dimval
     logical :: res
-
+    real(kind=4), dimension(:,:), allocatable :: h
+    integer(kind=MPI_OFFSET_KIND) :: starts(1,2), counts(1,2)
     ! check for input file
     call getcwd(cwd)
     ncfilename=trim(cwd)//ncfile_rel
@@ -25,101 +31,193 @@ program swe_mpi
         call exit(1)
     end if
 
-    call MPI_Init(ierror)
-    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror)
-    call MPI_Comm_size(MPI_COMM_WORLD, size, ierror)
+    call MPI_Init(ncstatus)
+    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ncstatus)
+    call MPI_Comm_size(MPI_COMM_WORLD, comm_size, ncstatus)
+
+    ! number of subgrids on side 
+    nsubgrid = nlen_subgrids(comm_size)
+    if (comm_size .ne. (nsubgrid*nsubgrid)) then
+        print *, "Number of MPI processes not square."
+        call exit(1)
+    end if
 
     ! find nearest neighbors
-    cell_neighbors = nearest_neighbors(rank, size) 
-
-    
+    cell_neighbors = nearest_neighbors(rank, comm_size) 
 
     ! open netcdf file for read
-    ierror = nf90mpi_open(mpi_comm=MPI_COMM_WORLD, path=ncfilename, omode=NF_NOWRITE, mpi_info=MPI_INFO_NULL, ncid=ncid)
-    if (ierror .ne. 0) print *, "nf90mpi_open error: ", ierror, ". Rank: ", rank
+    ncstatus = nf90mpi_open(mpi_comm=MPI_COMM_WORLD, path=ncfilename, omode=NF_NOWRITE, mpi_info=MPI_INFO_NULL, ncid=ncid)
+    call ncdf_check(ncstatus, "nf90mpi_open", rank)
 
+    ! get number, names, and values of dimensions
+    ncstatus = nfmpi_inq_ndims(ncid, ndims)  ! number of dimensions
+    call ncdf_check(ncstatus, "nfmpi_inq_ndims")
     
+    allocate(dimval(ndims))
+    allocate(dimnamei(ndims))
+    
+    do i=1,ndims
+        ncstatus = nfmpi_inq_dim(ncid, i, dimnamei(i), dimval(i))
+        call ncdf_check(ncstatus, "nfmpi_inq_dim")
+        if (dimnamei(i) .eq. "nx") then
+            nx = dimval(i)
+        else if (dimnamei(i) .eq. "ny") then
+            ny = dimval(i)
+        endif
+    end do
+    print *, "nx: ", nx, " ny: ", ny
+
+    local_nx = nx/nsubgrid
+    local_ny = ny/nsubgrid
+
+    ! read data subregion
+    starts(1,1) = local_nx*(mod(rank, nsubgrid)) + 1
+    starts(1,2) = local_ny*(mod(rank, nsubgrid)) + 1
+    counts(1,1) = local_nx
+    counts(1,2) = local_ny 
+
+    allocate(h(nx, ny)) 
+    i=1 ! h is only variable in source netcdf
+    ncstatus = nfmpi_get_vara_real_all(ncid, i , starts, counts, h)
+    call ncdf_check(ncstatus, "nfmpi_get_vara_real_all", rank)
+   
     ! close netcdf file
-    ierror = nf90mpi_close(ncid)
-    if (ierror .ne. 0) print *, "nf90mpi_close: ", ierror, ". Rank: ", rank
+    ncstatus = nf90mpi_close(ncid)
+    call ncdf_check(ncstatus, "nf90mpi_close", rank)
+
+    ! execute solver
+
+    ! write results
 
     if (rank .eq. 0) then
-        print *, "Program finished with ", size, " threads."
+        print *, "Program finished with ", comm_size, " threads."
     end if
     
-    call MPI_Finalize(ierror)
+    call MPI_Finalize(ncstatus)
     
     contains
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Function: nearest_neighbors
-!
-! Input:   i -- index of compute cell
-!          N -- total number of cells in square grid
-! Returns: derived-type that contains the index of the cell neighbor
-!          to the right, left, above and below 
-!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-pure function nearest_neighbors(i,N)
-    
-    implicit none
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ! Function: nlen_subgrids
+        ! Description: returns number of subgrids on side of in square grids
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        pure function nlen_subgrids(nprocs)
 
-    integer, intent(in) :: i, N
-    integer :: ni
-
-    ! return value: nearest neighbors
-    type(Neighbors) :: nearest_neighbors
-
-    ni = int(sqrt(real(N,kind=8)))
-
-    ! note: (i/ni)*ni = integer equivalent of floor(i/ni)*ni
-    nearest_neighbors%right = mod(i+1,ni) + (i/ni)*ni 
-    nearest_neighbors%left = mod(i+ni-1,ni) + (i/ni)*ni
-    nearest_neighbors%below = mod(i+N-ni, N)
-    nearest_neighbors%above = mod(i+N+ni, N)
-
+            implicit none
             
-end function nearest_neighbors
+            integer, intent(in) :: nprocs
+            integer :: nlen_subgrids
+
+            nlen_subgrids = int(sqrt(real(nprocs)))
+
+        end function nlen_subgrids
+
+
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ! Function: nearest_neighbors
+        ! Description: Returns struct with nearest neighbors on a cyclic grid
+        !
+        ! Input:   i -- index of compute cell
+        !          N -- total number of cells in square grid
+        ! Returns: derived-type that contains the index of the cell neighbor
+        !          to the right, left, above and below 
+        !
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        pure function nearest_neighbors(i,N)
+            
+            implicit none
+
+            integer, intent(in) :: i, N
+            integer :: ni
+
+            ! return value: nearest neighbors
+            type(Neighbors) :: nearest_neighbors
+
+            ni = int(sqrt(real(N,kind=8)))
+
+            ! note: (i/ni)*ni = integer equivalent of floor(i/ni)*ni
+            nearest_neighbors%right = mod(i+1,ni) + (i/ni)*ni 
+            nearest_neighbors%left = mod(i+ni-1,ni) + (i/ni)*ni
+            nearest_neighbors%below = mod(i+N-ni, N)
+            nearest_neighbors%above = mod(i+N+ni, N)
+
+        end function nearest_neighbors
+
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ! Function: ncdfcheck
+        ! Description:
+        !
+        ! Input: ncstatus - ncstatus from PnetCDF function call 
+        !        message - user informational message
+        !        rank (optional) - mpi processor rank 
+        !         
+        ! Returns: Exits program if ncstatus is not zero
+        !
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        subroutine ncdf_check(ncstatus, message, rank)
+       
+            implicit none
+            integer :: ncstatus
+            integer, optional :: rank
+            character(*) :: message
+            
+            if (ncstatus .ne. 0) then
+                if (present(rank)) then
+                    write (6, *) "PnetCDF procedure reported error: (" , ncstatus, "), rank ", rank, ": ", trim(message)   
+                else
+                    write (6, *) "PnetCDF procedure reported error: (", ncstatus, "): ", trim(message)
+                end if
+            end if
+        
+        end subroutine ncdf_check
+
     
-subroutine derivative(nx, ny, x, dx)
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ! Function: derivative
+        ! Description:
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    implicit none 
+        subroutine derivative(nx, ny, x, dx)
 
-    integer, intent(in) :: nx, ny            ! field dims
+            implicit none 
 
-    ! Actually, this MPI task only "owns" x(1:nx,1:ny)
-    real, dimension(0:nx+1,0:ny+1), intent(in) :: x     ! field
-    real, dimension(0:nx+1,0:ny+1), intent(out) :: dx   ! x-deriv of field
+            integer, intent(in) :: nx, ny            ! field dims
 
-    integer :: i, j
+            ! Actually, this MPI task only "owns" x(1:nx,1:ny)
+            real, dimension(0:nx+1,0:ny+1), intent(in) :: x     ! field
+            real, dimension(0:nx+1,0:ny+1), intent(out) :: dx   ! x-deriv of field
+
+            integer :: i, j
 
 
-    ! In setup routine
-    ! *) NPX - num tasks in x-direction (initially, sqrt(comm_size))
-    ! *) NPY - num tasks in y-direction (initially, sqrt(comm_size))
-    ! *) PX  - x-coord in decomp = (comm_rank % NPX)
-    ! *) PY  - y-coord in decomp = (comm_rank / NPY)
+            ! In setup routine
+            ! *) NPX - num tasks in x-direction (initially, sqrt(comm_size))
+            ! *) NPY - num tasks in y-direction (initially, sqrt(comm_size))
+            ! *) PX  - x-coord in decomp = (comm_rank % NPX)
+            ! *) PY  - y-coord in decomp = (comm_rank / NPY)
 
-    !
-    ! Do "halo exchange" here to ensure that
-    ! x(0,:) and x(nx+1,:) are updated from neighboring processors
-    !
-    ! 1) Every MPI task needs to know its location in the overall decomposition (PX, PY)
-    ! 2) For the halo exchange, updated left column from processor PX-1
-    !                           updated right column from processor PX+1
+            !
+            ! Do "halo exchange" here to ensure that
+            ! x(0,:) and x(nx+1,:) are updated from neighboring processors
+            !
+            ! 1) Every MPI task needs to know its location in the overall decomposition (PX, PY)
+            ! 2) For the halo exchange, updated left column from processor PX-1
+            !                           updated right column from processor PX+1
 
-    ! call MPI_Irecv(..., x(0,:), ...)     ! recv from task to left
-    ! call MPI_Irecv(..., x(nx+1,:), ...)  ! recv from task to right
+            ! call MPI_Irecv(..., x(0,:), ...)     ! recv from task to left
+            ! call MPI_Irecv(..., x(nx+1,:), ...)  ! recv from task to right
 
-    ! call MPI_Isend(..., x(nx,:), ...)    ! send to task to right
-    ! call MPI_Isend(..., x(1,:), ...)     ! send to task to left
+            ! call MPI_Isend(..., x(nx,:), ...)    ! send to task to right
+            ! call MPI_Isend(..., x(1,:), ...)     ! send to task to left
 
-    do j = 1, ny
-    do i = 1, nx
-       dx(i,j) = x(i+1,j) - x(i-1,j)
-    end do
-    end do
-end subroutine derivative
+            do j = 1, ny
+            do i = 1, nx
+               dx(i,j) = x(i+1,j) - x(i-1,j)
+            end do
+            end do
+        end subroutine derivative
 
 end program
 
