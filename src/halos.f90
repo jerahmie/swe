f9bac11686884ecdf5a9e08b0c0d6332b334847b
Jerahmie W. Radder <jerahmie@gmail.com>
2023-09-24 23:22:24 -0500

    Included message passing for neighboring X-regions

--- src/halos.f90
+++ src/halos.f90
@@ -8,16 +8,16 @@ program swe_mpi
 
     type(Neighbors) :: cell_neighbors
     integer :: num_args
-    integer :: rank, comm_size, ncid, ncstatus
-    integer :: i, j, ndims, dimids(2), varid
+    integer :: comm_rank, comm_size, ncid, ncstatus
+    integer :: i, j, it,  ndims, dimids(2), varid
     integer :: nx, ny, local_nx, local_ny, nsubgrid
-    !integer, dimension(1024) :: vardimids
     character(255) :: ncfile_input, ncfile_output
     character(len=32), dimension(:), allocatable :: dimnamei ! varname
     integer(kind=MPI_OFFSET_KIND), dimension(:), allocatable :: dimval
     logical :: res
     real(kind=4), dimension(:,:), allocatable :: h, dhx
     integer(kind=MPI_OFFSET_KIND) :: starts(2), counts(2)
+    integer, parameter :: NT = 25
   
     ! get filename from command line
     num_args = command_argument_count()
@@ -36,7 +36,7 @@ program swe_mpi
     end if
 
     call MPI_Init(ncstatus)
-    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ncstatus)
+    call MPI_Comm_rank(MPI_COMM_WORLD, comm_rank, ncstatus)
     call MPI_Comm_size(MPI_COMM_WORLD, comm_size, ncstatus)
 
     ! number of subgrids on side 
@@ -47,13 +47,13 @@ program swe_mpi
     end if
 
     ! find nearest neighbors
-    cell_neighbors = nearest_neighbors(rank, comm_size) 
+    cell_neighbors = nearest_neighbors(comm_rank, comm_size) 
 
     ! TODO: move load data calls to module
     ! open netcdf file for read
     ncstatus = nf90mpi_open(mpi_comm=MPI_COMM_WORLD, path=ncfile_input, &
                             omode=NF_NOWRITE, mpi_info=MPI_INFO_NULL, ncid=ncid)
-    call ncdf_check(ncstatus, "nf90mpi_open", rank)
+    call ncdf_check(ncstatus, "nf90mpi_open", comm_rank)
 
     ! get number, names, and values of dimensions
     ncstatus = nfmpi_inq_ndims(ncid, ndims)  ! number of dimensions
@@ -77,8 +77,8 @@ program swe_mpi
     local_ny = ny/nsubgrid
 
     ! read data subregion
-    starts(1) = local_nx*mod(rank,nsubgrid)+1
-    starts(2) = local_ny*(rank*local_ny/nx) + 1
+    starts(1) = local_nx*mod(comm_rank,nsubgrid)+1
+    starts(2) = local_ny*(comm_rank*local_ny/nx) + 1
     counts(1) = local_nx
     counts(2) = local_ny
 
@@ -87,43 +87,45 @@ program swe_mpi
 
     i=1 ! h is only variable in source netcdf
     ncstatus = nfmpi_get_vara_real_all(ncid, i , starts, counts, h)
-    call ncdf_check(ncstatus, "nfmpi_get_vara_real_all", rank)
+    call ncdf_check(ncstatus, "nfmpi_get_vara_real_all", comm_rank)
    
     ! close netcdf file
     ncstatus = nf90mpi_close(ncid)
-    call ncdf_check(ncstatus, "nf90mpi_close", rank)
+    call ncdf_check(ncstatus, "nf90mpi_close", comm_rank)
 
     ! execute solver
-    call derivative(local_nx, local_ny, h, dhx)
+    do it=1,NT
+        call derivative(local_nx, local_ny, h, dhx)
+    end do
 
     ! write results
     ! TODO: move to output module 
     ! create new netcdf file for read
     ncstatus = nf90mpi_create(mpi_comm=MPI_COMM_WORLD, path=trim(ncfile_output), &
                               cmode=NF_CLOBBER, mpi_info=MPI_INFO_NULL, ncid=ncid)
-    call ncdf_check(ncstatus, "nf90mpi_open for write", rank)
+    call ncdf_check(ncstatus, "nf90mpi_open for write", comm_rank)
     ncstatus = nfmpi_def_dim(ncid, "ny", int(ny, kind=MPI_OFFSET_KIND), dimids(2))
-    call ncdf_check(ncstatus, "nf90mpi_def_dim for write", rank)
+    call ncdf_check(ncstatus, "nf90mpi_def_dim for write", comm_rank)
     ncstatus = nfmpi_def_dim(ncid, "nx", int(nx, kind=MPI_OFFSET_KIND), dimids(1))
-    call ncdf_check(ncstatus, "nf90mpi_def_dim for write", rank)
+    call ncdf_check(ncstatus, "nf90mpi_def_dim for write", comm_rank)
     
     ncstatus = nfmpi_def_var(ncid, "var", NF90_FLOAT, 2, dimids, varid)
-    call ncdf_check(ncstatus, "nf90mpi_def_var for write", rank)
+    call ncdf_check(ncstatus, "nf90mpi_def_var for write", comm_rank)
 
     ncstatus = nf90mpi_enddef(ncid)
-    call ncdf_check(ncstatus, "nf90mpi_enddef for write", rank)
-    if (rank .eq. 0) then
+    call ncdf_check(ncstatus, "nf90mpi_enddef for write", comm_rank)
+    if (comm_rank .eq. 0) then
         print*, "dimids: ", dimids, " varid: ", varid
     endif
     
     ncstatus = nfmpi_put_vara_real_all(ncid, i, starts, counts, h)
-    call ncdf_check(ncstatus, "nfmpi_put_vara_real_all for write", rank)
+    call ncdf_check(ncstatus, "nfmpi_put_vara_real_all for write", comm_rank)
     
     ! close netcdf file
     ncstatus = nf90mpi_close(ncid)
-    call ncdf_check(ncstatus, "nf90mpi_close for write", rank)
+    call ncdf_check(ncstatus, "nf90mpi_close for write", comm_rank)
 
-    if (rank .eq. 0) then
+    if (comm_rank .eq. 0) then
         print *, "Program finished with ", comm_size, " threads."
     end if
     
@@ -131,29 +133,28 @@ program swe_mpi
     
     contains
 
-
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         ! Function: ncdfcheck
         ! Description:
         !
         ! Input: ncstatus - ncstatus from PnetCDF function call 
         !        message - user informational message
-        !        rank (optional) - mpi processor rank 
+        !        comm_rank (optional) - mpi processor rank 
         !         
         ! Returns: Exits program if ncstatus is not zero
         !
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        subroutine ncdf_check(ncstatus, message, rank)
+        subroutine ncdf_check(ncstatus, message, comm_rank)
        
             implicit none
             integer :: ncstatus
-            integer, optional :: rank
+            integer, optional :: comm_rank
             character(*) :: message
             
             if (ncstatus .ne. 0) then
-                if (present(rank)) then
+                if (present(comm_rank)) then
                     write (6, *) "PnetCDF procedure reported error: (" , &
-                        ncstatus, "), rank ", rank, ": ", trim(message)   
+                        ncstatus, "), comm_rank ", comm_rank, ": ", trim(message)   
                 else
                     write (6, *) "PnetCDF procedure reported error: (", &
                         ncstatus, "): ", trim(message)
@@ -167,26 +168,27 @@ program swe_mpi
         ! Function: derivative
         ! Description:
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+    
         subroutine derivative(nx, ny, x, dx)
-
             implicit none 
 
             integer, intent(in) :: nx, ny            ! field dims
-
+            integer :: PX, pleft, pright
             ! Actually, this MPI task only "owns" x(1:nx,1:ny)
             real, dimension(0:nx+1,0:ny+1), intent(in) :: x     ! field
             real, dimension(0:nx+1,0:ny+1), intent(out) :: dx   ! x-deriv of field
-
-            integer :: i, j
-
-
+            integer  :: stat(MPI_STATUS_SIZE)
+            integer :: i, j, tag, ierr, request
+            
             ! In setup routine
             ! *) NPX - num tasks in x-direction (initially, sqrt(comm_size))
             ! *) NPY - num tasks in y-direction (initially, sqrt(comm_size))
             ! *) PX  - x-coord in decomp = (comm_rank % NPX)
             ! *) PY  - y-coord in decomp = (comm_rank / NPY)
 
+            PX = nlen_subgrids(comm_size)
+            pright = mod(comm_rank+1,PX) + (comm_rank/PX)*PX  
+            pleft = mod(comm_rank+PX-1,PX) + (comm_rank/PX)*PX  
             !
             ! Do "halo exchange" here to ensure that
             ! x(0,:) and x(nx+1,:) are updated from neighboring processors
@@ -200,6 +202,16 @@ program swe_mpi
 
             ! call MPI_Isend(..., x(nx,:), ...)    ! send to task to right
             ! call MPI_Isend(..., x(1,:), ...)     ! send to task to left
+             
+            call MPI_Irecv(x(0,:), nx, MPI_REAL, pleft, comm_rank, &
+                           MPI_COMM_WORLD, request, ierr)
+            call MPI_Irecv(x(nx+1,:), nx, MPI_REAL, pright, comm_rank, &
+                           MPI_COMM_WORLD, request, ierr)
+            call MPI_Isend(x(nx,:), nx, MPI_REAL, pright, comm_rank, &
+                           MPI_COMM_WORLD, request, ierr)
+            call MPI_Isend(x(1,:), nx, MPI_REAL, pleft, comm_rank, &
+                           MPI_COMM_WORLD, request, ierr)
+            call MPI_Wait(request, stat, ierr)
 
             do j = 1, ny
                 do i = 1, nx
